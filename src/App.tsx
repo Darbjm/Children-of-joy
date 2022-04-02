ba70f7aa60aa34c6ff3db16f12ee4e99eb026b96
James Darby <james.michael.darby@gmail.com>
2022-04-03 00:13:37 +0100

    added in hidden image

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import face from './assets/face.png';
 import './App.css';
 
 function App() {
   const [isClicked, setIsClicked] = useState(false)
 
+  useEffect(() => {
+    setTimeout(() => setIsClicked(true), 10000)
+  },[])
+
   return (
     <div className="App">
+      <div className={`page ${isClicked ? "view" : "hidden"}`}/>
         {isClicked ? <>
-            <div className='page'/>
             <a className='pay' href='https://buy.stripe.com/5kA2bh5xN6XG8gwbII' target="_blank" rel="noopener noreferrer">JOIN THE CULT</a>
             </>
           : <button type='button' className='enter' onClick={() => 
