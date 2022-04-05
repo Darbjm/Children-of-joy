33c1c097027712b352839cd3998d3e0588197bdc
James Darby <james.michael.darby@gmail.com>
2022-04-05 23:17:38 +0100

    updated info page

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,26 @@ import './App.css';
 
 function App() {
   const [isClicked, setIsClicked] = useState(false)
+  const [isSliding, setIsSliding] = useState(false)
 
   useEffect(() => {
     setTimeout(() => setIsClicked(true), 10000)
+    setTimeout(() => setIsSliding(true), 5000)
   },[])
 
   return (
     <div className="App">
-      <div className={`page ${isClicked ? "view" : "hidden"}`}/>
-        {isClicked ? <>
-            <a className='pay' href='https://buy.stripe.com/5kA2bh5xN6XG8gwbII' target="_blank" rel="noopener noreferrer">JOIN THE CULT</a>
-            </>
-          : <button type='button' className='enter' onClick={() => 
+      <div className={`page ${isClicked ? "view" : "hidden"}`}>
+        {isClicked && 
+            <a className={`pay ${isSliding ? "slide-up" : ""}`} href='https://buy.stripe.com/5kA2bh5xN6XG8gwbII' target="_blank" rel="noopener noreferrer">JOIN THE CULT</a>}
+        </div>
+        {!isClicked &&
+        <button type='button' className='enter' onClick={() => 
           setIsClicked(true)}>
             <div className='rotate'>
               <img  src={face} className="App-logo" alt="face" />
             </div>
-        </button>
-        }
+        </button>}
     </div>
   );
 }
